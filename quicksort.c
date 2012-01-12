06f949e65a5edf022221d8d4391bd898f9b8cfb9
kapcino <kapcino@gmail.com>
2012-01-12 09:31:47 +0800

    Try to fix the first method, not done.

--- quicksort.c
+++ quicksort.c
@@ -7,8 +7,8 @@ void quick_sort(int a[], int lo, int hi)
 
   do
     {
-      while(a[i]<a[pi]) i++;
-      while(a[j]>a[pi]) j--;
+      while(a[i]<=a[pi]) i++;
+      while(a[j]>=a[pi]) j--;
 
       if(i<=j)
 	{
@@ -21,9 +21,13 @@ void quick_sort(int a[], int lo, int hi)
 	}
     } 
   while(i<=j);
+  
+  h=a[j];
+  a[j]=a[pi];
+  a[pi]=h;
     
-  if(lo<j) quick_sort(a, lo, j);
-  if(i<hi) quick_sort(a, i, hi);
+  if(lo<j-1) quick_sort(a, lo, j-1);
+  if(j+1<hi) quick_sort(a, j+1, hi);
 }
 
 void quicksort(int a[], int len)
@@ -66,7 +70,6 @@ void recursive_quicksort(int a[], int lo, int hi)
   if(lo < hi)
     {
       pivot = partition(a, lo, hi);
-      printf("%d\n", pivot);
       recursive_quicksort(a, lo, pivot-1);
       recursive_quicksort(a, pivot+1, hi);
     }
@@ -83,7 +86,7 @@ int main(int argc, char* argv[])
   int a[]={1, 8, 4, 3, -4, 93, 23, 43, 7};
   int len = sizeof(a)/sizeof(a[0]);
   print(a, len);
-  quicksort2(a, len);
+  quicksort(a, len);
   print(a, len);
 
   return 0;
